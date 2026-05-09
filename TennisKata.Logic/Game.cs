48aa7b415c8b49c58610b5e2ec1bfc92f62f1b78
Ioannis Siakavaras <giannisiakavaras@gmail.com>
2026-05-10 00:34:08 +0300

    feat: handle PointWonBy for player two

--- TennisKata.Logic/Game.cs
+++ TennisKata.Logic/Game.cs
@@ -5,6 +5,7 @@ public class Game
     private readonly Player _playerOne;
     private readonly Player _playerTwo;
     private int _pointsPlayerOne;
+    private int _pointsPlayerTwo;
 
     public Game(Player playerOne, Player playerTwo)
     {
@@ -19,6 +20,11 @@ public class Game
             return "Fifteen-Love";
         }
 
+        if (_pointsPlayerTwo == 1)
+        {
+            return "Love-Fifteen";
+        }
+
         return "Love-All";
     }
 
@@ -27,6 +33,12 @@ public class Game
         if (player == _playerOne)
         {
             _pointsPlayerOne++;
+            return;
+        }
+
+        if (player == _playerTwo)
+        {
+            _pointsPlayerTwo++;
         }
     }
 }
