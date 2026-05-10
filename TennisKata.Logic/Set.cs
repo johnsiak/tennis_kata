758f08022b66d1d0b5a1ddfbad368ce6f883059f
Ioannis Siakavaras <giannisiakavaras@gmail.com>
2026-05-10 21:40:13 +0300

    feat: implement PointWonBy delegating to current game and updating games

--- TennisKata.Logic/Set.cs
+++ TennisKata.Logic/Set.cs
@@ -4,15 +4,33 @@ public class Set
 {
     private readonly Player _playerOne;
     private readonly Player _playerTwo;
+    private int _gamesPlayerOne;
+    private int _gamesPlayerTwo;
+    private IGame _currentGame;
 
     public Set(Player playerOne, Player playerTwo)
     {
         _playerOne = playerOne;
         _playerTwo = playerTwo;
+        _currentGame = new Game(_playerOne, _playerTwo);
+    }
+
+    public void PointWonBy(Player player)
+    {
+        _currentGame.PointWonBy(player);
+
+        if (_currentGame.IsOver())
+        {
+            if (_currentGame.Winner() == _playerOne)
+            {
+                _gamesPlayerOne++;
+                _currentGame = new Game(_playerOne, _playerTwo);
+            }
+        }
     }
 
     public string GetScore()
     {
-        return "0-0";
+        return $"{_gamesPlayerOne}-{_gamesPlayerTwo}";
     }
 }
