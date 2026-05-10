39000f1d189b75bc154f28161a551f3135054be7
Ioannis Siakavaras <giannisiakavaras@gmail.com>
2026-05-10 15:23:34 +0300

    test: player two wins a point tiebreak score is 0-1

--- TennisKata.Tests/TiebreakGameTests.cs
+++ TennisKata.Tests/TiebreakGameTests.cs
@@ -24,4 +24,14 @@ public class TiebreakGameTests
 
         Assert.Equal("1-0", game.GetScore());
     }
+
+    [Fact]
+    public void PlayerTwoWinsAPointTiebreakScoreIsZeroOne()
+    {
+        var game = new TiebreakGame(_playerOne, _playerTwo);
+
+        game.PointWonBy(_playerTwo);
+
+        Assert.Equal("0-1", game.GetScore());
+    }
 }
